bdf2e22d82396d3f96479249ea29db05d3de5a9b
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-09-13 15:39:19 +0700

    kek

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "1891144689:AAEquasTMejOBaBCMYvSVsxMVZB0f30Krgg"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "Ironblood"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/Ryomen-Sukuna/GitGram"
